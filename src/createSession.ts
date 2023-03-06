e8e9b7a5250cc5604bc4fd7808f257f9eefb31b3
Odunosh Moses <Odunoshomoses@gmail.com>
2023-03-06 16:23:04 +0000

    ui setup for create and join game

---
+++ src/createSession.ts
@@ -0,0 +1,9 @@
+import { nanoid } from 'nanoid'
+
+function generateId(){
+    console.log(module.id = nanoid())
+}
+
+export class createSession {
+    
+}
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+//import { setupCounter } from '.'
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+
+  ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
+    <React.StrictMode>
+    <App />
+    </React.StrictMode>
+    )
+
+//setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
