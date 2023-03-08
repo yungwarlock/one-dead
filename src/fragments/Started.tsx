1871bacbabbf54c2c74658dc1857a1c9c12c651b
Damian Akpan <dami.akpan@gmail.com>
2023-03-08 10:26:06 +0100

    Implemented initial UI for main game

--- src/fragments/Started.tsx
+++ src/fragments/Started.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import CodeInputField from "../components/CodeInputField";
 
 
 const Started = (): JSX.Element => {
+  const [currentCode, setCurrentCode] = React.useState<string>("");
+
+  React.useEffect(() => {
+    console.log(currentCode);
+  }, [currentCode]);
+
+
   return (
-    <div>Started</div>
+    <div>
+      <div id="result">1 dead 1 Injured</div>
+
+      <div>
+        <CodeInputField onChange={(val) => setCurrentCode(val)} />
+      </div>
+
+      <div>
+
+      </div>
+    </div>
   );
 }
 
